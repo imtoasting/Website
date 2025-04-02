bc6183ba48a4b4dee3a8a8cf4d9d476d07d9e910
Sreeman Reddy <139124397+sreemanrp@users.noreply.github.com>
2025-04-02 12:38:35 +0530

    Add files via upload

---
+++ framer/logofy/src/main.tsx
@@ -0,0 +1,22 @@
+import "framer-plugin/framer.css"
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { App } from "./App.tsx"
+import { framer } from "framer-plugin"
+
+const root = document.getElementById("root")
+if (!root) throw new Error("Root element not found")
+
+void framer.showUI({
+    position: "top right",
+    width: 300,
+    height: 330,
+    resizable: true,
+})
+
+ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
